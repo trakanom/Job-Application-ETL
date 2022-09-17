bd8256a9f26b29fc9661dad446616d86c37e521d
Trakanom <mika.wisener@gmail.com>
2022-09-17 12:26:12 -0700

    initial commit

---
+++ email_scraper.py
@@ -0,0 +1,287 @@
+import os
+import re
+import csv
+import urllib.request
+
+
+class HTML_Scraper:
+    def __init__(self, working_directory):
+        self.working_dir = working_directory
+        self.read_filenames = set()
+        self.scraped_htmls = set()
+        self.JobID=jobID()
+        self.applied_jobs = []
+        self.posted_jobs = []
+
+    def scan_new_application(self, file_name, file_contents, debug=False):
+        if debug:
+            print("~~DEBUG~~:")
+
+        #Get Date
+        date_filter=re.compile('Date: .+\(UTC\)', flags=re.DOTALL)
+        date = re.search(date_filter,file_contents).group().strip("Date: ")
+        
+
+        #Get Title & Company
+        prefix = "You applied for "
+        m = re.compile(f"{prefix}.+\.eml",flags=re.DOTALL)
+        if re.match(m, file_name):
+            if debug:
+                print(f"easy way worked!")
+            Subject = file_name[:-4]
+            Position, Company = Subject[len(prefix):].split(" at ")
+        else:
+            if debug:
+                print(f"easy way failed:::{file_name}")
+            try:
+                p=re.compile(r"Subject: You applied for (.+)MIME", flags=re.DOTALL)
+                match = re.search(p,file_contents)
+                Subject = match.group().replace('\n','').strip('MIME')
+                prefix = "Subject: You applied for"
+                Position, Company = Subject[len(prefix):].split(" at ")
+            except:
+                p=re.compile(r"Subject: (.+)MIME", flags=re.DOTALL)
+                match = re.search(p,file_contents)
+                if debug:
+                    print("It broke. ",file_name)
+                Subject = match.group().replace('\n','').replace('_'," ").strip('MIME')
+                Position, Company = Subject[25:].split(" at ")
+
+        #Get Job URL
+        h=re.compile('View job: https:(\S+=?)', flags=re.DOTALL)
+        j=re.compile('&jobId=\w+')
+        match2 = re.search(h,file_contents)
+        match3 = re.search(j, match2.group())
+        stored2 = match3.group().strip('&jobId=')[2:]
+        JobURL = f'https://www.linkedin.com/jobs/view/{stored2}/'
+        
+        if debug:
+            if m:
+                print(f"SUBJECT: {Subject}")
+            if match2:
+                print(f"POST_ID: {stored2}")
+                print(f'POST_URL: https://www.linkedin.com/jobs/view/{stored2}/')
+            print(f"DATE: {date}")
+        data = {
+            'jobID':self.JobID.iter(),
+            'file_name':file_name,
+            'subject':Subject,
+            'position': Position,
+            'company': Company,
+            'postURL': JobURL,
+            'date_applied': date,
+            'date_viewed':None,
+            'date_rejected':None,
+        }
+        if debug:
+            print("DATA: ",data)
+        return data
+
+    def scan_application_viewed(self, file_name, file_contents, debug=False):
+        # Definitely can consolidate all these categories into one function. Debugging first, though.
+        date_filter=re.compile('Date: .+\(UTC\)', flags=re.DOTALL)
+        date = re.search(date_filter,file_contents).group().strip("Date: ")
+        Subject = file_name.replace("..",".")[:-4]
+        prefix = "Your application for "
+        m = re.compile(f"{prefix}.+\.eml",flags=re.DOTALL)
+        if re.match(m, file_name):
+            if debug:
+                print(f"easy way worked!")
+            Position, Company = Subject[len(prefix):].split(" was viewed by ")
+        data = {
+            'file_name':file_name,
+            'subject':Subject,
+            'position': Position,
+            'company': Company,
+            'date_viewed': date,
+        }
+        return data
+
+    def scan_application_rejected(self, file_name, file_contents, debug=False):
+        # Definitely can consolidate all these categories into one function. Debugging first, though.
+        date_filter=re.compile('Date: .+\(UTC\)', flags=re.DOTALL)
+        date = re.search(date_filter,file_contents).group().strip("Date: ")
+        Subject = file_name[:-4]
+        prefix = "Your application to "
+        m = re.compile(f"{prefix}.+\.eml",flags=re.DOTALL)
+        if re.match(m, file_name):
+            if debug:
+                print(f"easy way worked!")
+            Position, Company = Subject[len(prefix):].split(" at ")
+        data = {
+            'file_name':file_name,
+            'subject':Subject,
+            'position': Position,
+            'company': Company,
+            'date_rejected': date,
+        }
+        return data
+
+    def add_new_entries(self, payload, type, debug=False):
+        if debug:
+            print(payload)
+        if type=="applied":
+            self.applied_jobs.append(payload)
+        else:
+            if debug:
+                print(self.applied_jobs)
+            for index, application in enumerate(self.applied_jobs):
+                if debug:
+                    print("APP: ",application)
+                    print("app",application['company'],".")
+                    print("pay",payload['company'],".")
+                    print("app",application['position'],".")
+                    print("pay",payload['position'],".")
+                if application['company']==payload['company']:
+                    if debug:
+                        print("Checks out")
+                    if application['position']==payload['position']:
+                        if type=="rejected":
+                            self.applied_jobs[index]['date_rejected']=payload['date_rejected']
+                        if type=="viewed":
+                            self.applied_jobs[index]['date_viewed']=payload['date_viewed']
+                        if debug:
+                            print("Checks out")
+                            print(self.applied_jobs[index])
+                        break
+                else:
+                    pass
+                    if debug:
+                        print("match error")
+
+        self.read_filenames.add(payload['file_name'])
+
+    def update_from_local(self, data_directory=None, debug=False): 
+        '''
+        Searches through data_directory for newly downloaded job listings in HTML, then places the places them into database via JSON.
+        '''
+        data_directory = self.working_dir if data_directory==None else data_directory
+        #get list of all html files in directory as all_files
+        #find the left outer join between all_files and set(self.read_filenames.values())
+        
+        file_list = set(os.listdir(data_directory))
+        unread_files = list(file_list.difference(self.read_filenames)).sort()
+        
+        for file in unread_files:
+            eml_file = open(self.working_dir+file, 'r', encoding='utf-8')
+            file_contents = eml_file.read()
+            eml_file.close()
+            
+            application_filter = re.compile("X-LinkedIn-Template: jobs_applicant_applied")
+            viewed_filter = re.compile("X-LinkedIn-Template: email_jobs_job_application_viewed")
+            rejected_filter = re.compile("X-LinkedIn-Template: email_jobs_application_rejected")
+            
+            #rudimentry classifier
+            if re.search(application_filter,file_contents):
+                scanned_info = self.scan_new_application(file,file_contents,debug)
+                self.add_new_entries(scanned_info,"applied")
+                if debug:
+                    print("SUCCESS: Added \"{}\" file to scanned:'{}' @ {}".format(file,scanned_info['position'], scanned_info['company']),"\n")
+            elif re.search(viewed_filter,file_contents):
+                scanned_info = self.scan_application_viewed(file,file_contents,debug)
+                self.add_new_entries(scanned_info,"viewed")
+            elif re.search(rejected_filter,file_contents):
+                scanned_info = self.scan_application_rejected(file,file_contents,debug)
+                self.add_new_entries(scanned_info,"rejected")
+            else:
+                print("Error: No matches ({file})")
+
+    def export_to_file(self, export_dir=None):
+        # print(self.applied_jobs)
+        applied_jobs = self.applied_jobs
+        applied_keys = applied_jobs[0].keys()
+        export_dir=self.working_dir+'output_files' if export_dir is None else export_dir
+        with open(f'{export_dir}\\job_applications.csv','w',encoding='utf-8-sig',newline='') as output_file:
+            dict_writer = csv.DictWriter(output_file, applied_keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(applied_jobs)
+        
+        with open(f'{export_dir}\\read_files.py','w',encoding='utf-8-sig',newline='') as output_file:
+            output_file.write("read_files={")
+            for entry in self.read_filenames:
+                output_file.write("\""+entry+"\",")
+            output_file.write("}")
+
+    def import_from_file(self):
+        #In progress
+        # from read_files import read_files
+        # self.read_filenames=read_files
+        # print(len(self.read_filenames))
+        pass
+
+    def download_posting_html(self, download_directory=None, debug=False):
+        # stored = []
+        download_dir=self.working_dir+'output_files\\scraped_data' if download_directory is None else download_directory
+        for application in self.applied_jobs:
+            jobID = application['jobID']
+            postURL = application['postURL']
+            try:
+                urllib.request.urlretrieve(postURL, f"{download_directory}Job_{jobID}.html")
+                if debug:
+                    print(f"Success! (ID:{jobID}={postURL})")
+            except:
+                with open(f"{download_directory}\\Job_{jobID}.html",'w',encoding='utf-8-sig',newline='') as output_file:
+                    output_file.write("Null")
+                    if debug:
+                        print(f"Error 404! (ID:{jobID}={postURL}) ")
+
+    def parse_jobs_html(self,output_filepath= None, input_directory=None,debug=False):
+        input_dir=self.working_dir+"\\scraped_data" if input_directory is None else input_directory
+        output_dir=self.working_dir+"\\output_files\\html_data.csv" if output_dir is None else output_dir
+        file_list = set(os.listdir(input_dir))
+        unread_files = list(file_list.difference(self.scraped_htmls))
+        unread_files.sort()
+        data = []
+        for file in unread_files:
+            #Prolly wanna use an html parser at some point.
+            jobID = file.strip("\.html"|"Job_")
+            eml_file = open(input_dir+file, 'r', encoding='utf-8')
+            file_contents = eml_file.read()
+            eml_file.close()
+            
+            #parse file_contents
+            datum = {
+                'JobID':jobID,
+                'raw':file_contents,
+            }
+            if debug:
+                print(datum)
+            data.append(datum)
+        if debug:
+            print(f"ALL HTML DATA: {data}")
+        return data
+
+    def update_db(self):
+        pass
+
+class jobID:
+    def __init__(self):
+        #starting at -1 for testing purposes.
+        #eventually add reference to storage and continue count.
+        self.Value = -1
+    def iter(self):
+        self.Value+=1
+        return self.Value
+    def value(self):
+        return self.Value
+    def next(self):
+        return self.Value+1
+    def prev(self):
+        return self.Value-1
+
+if __name__=='__main__':
+    h = HTML_Scraper('\\data\\')
+    h.update_from_local(debug=False)
+    h.export_to_file()
+    h.download_posting_html()
+    
+    # h.import_from_file()
+    # print(h.posted_jobs)
+    # # Debugging:
+    # print(h.scan_new_application('You applied for Supply Chain Data Analyst 100% Remote Fortune 60 Co Direct Hire Salary up to $110K per annum at Confidential.eml')) #(long subject name and outputs)
+    # h.scan_new_application('You applied for FULLY REMOTE- Software Engineer (Python_Django) at CyberCoders.eml',debug=True) #(symbols)
+    # h.scan_new_application('You applied for Data Analyst at Fēnom Digital.eml',debug=True) #(utf-8 encoding req)
+    
+    # print(h.applied_jobs.values(), sep="\n")
+    # "You applied for Python Developer at Comrise.eml" #(e goes missing)
+    # "Your application to Python Developer at Comrise.eml"
